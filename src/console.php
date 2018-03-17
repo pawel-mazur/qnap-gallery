5bd29de20eece663df2a0f3b0a94ff0d1c21ca41
Paweł Mazur <pawel.mazur@netteam.pl>
2018-03-17 16:35:38 +0100

    Dump bazy danych

--- src/console.php
+++ src/console.php
@@ -10,13 +10,47 @@ $console = new Application('My Silex Application', 'n/a');
 $console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
 $console->setDispatcher($app['dispatcher']);
 $console
-    ->register('my-command')
+    ->register('database:dump')
     ->setDefinition(array(
-        // new InputOption('some-option', null, InputOption::VALUE_NONE, 'Some help'),
+        new InputArgument('file', InputArgument::OPTIONAL, 'Dump file', 'var/dump.sql'),
     ))
-    ->setDescription('My command description')
+    ->setDescription('Restore database dump')
     ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
-        // do something
+        $file = $input->getArgument('file');
+
+        if(shell_exec("which mysqldump")){
+            $mysqldump = 'mysqldump';
+        } else {
+            $mysqldump = '/usr/local/mariadb/bin/mysqldump';
+        }
+
+        exec(sprintf('%s --host %s --port %s --user %s --password="%s" --single-transaction %s --result-file %s',
+            $mysqldump,
+            $app['database.host'],
+            $app['database.port'],
+            $app['database.user'],
+            $app['database.password'],
+            $app['database.dbname'],
+            $file
+        ));
+    })
+;
+$console
+    ->register('database:restore')
+    ->setDefinition(array(
+         new InputArgument('file', InputArgument::OPTIONAL, 'Dump file', 'var/dump.sql'),
+    ))
+    ->setDescription('Restore database dump')
+    ->setCode(function (InputInterface $input, OutputInterface $output) use ($app) {
+        $file = $input->getArgument('file');
+        exec(sprintf('mysql --host %s --port %s --user %s --password="%s" %s < %s',
+            $app['database.host'],
+            $app['database.port'],
+            $app['database.user'],
+            $app['database.password'],
+            $app['database.dbname'],
+            $file
+        ));
     })
 ;
 
